fef984201b097a2b6e38a29df1a2f70d42d45354
geraud77 <vgeraud77@gmail.com>
2026-05-22 00:42:22 +0100

    speedtrack

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { ToastProvider } from './context/toastContext.tsx';
-import HabitProvider from './context/habitProvider.tsx';
-import { NavProvider } from './context/navigationContext.tsx';
-import { AppModeProvider } from './context/appModeContext.tsx';
-import { CommandPaletteProvider } from './context/commandPaletteContext.tsx';
-import { OnboardingProvider } from './context/onboardingContext.tsx';
-import { ThemeProvider } from './context/themeContext.tsx';
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ToastProvider } from "./context/toastContext.tsx";
+import HabitProvider from "./context/habitProvider.tsx";
+import { NavProvider } from "./context/navigationContext.tsx";
+import { AppModeProvider } from "./context/appModeContext.tsx";
+import { CommandPaletteProvider } from "./context/commandPaletteContext.tsx";
+import { OnboardingProvider } from "./context/onboardingContext.tsx";
+import { ThemeProvider } from "./context/themeContext.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <SpeedInsights />
     <ThemeProvider>
       <ToastProvider>
         <HabitProvider>
